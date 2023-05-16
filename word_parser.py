1f088dbed1e0eb5b403235aeb33a0dfd8c6a89fb
wobertt <robertchung1462@gmail.com>
2023-05-16 18:35:08 -0400

    Create word_parser.py

---
+++ word_parser.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+
+
+class Word:
+    def __init__(self, letters, wpm, is_error):
+        self.letters = letters
+        self.wpm = wpm
+        self.is_error = is_error
+
+    
+    def __repr__(self):
+        return f'{self.letters}: wpm={self.wpm}, typo={self.is_error}'
+
+
+def get_word_from_div(word_div):
+    letter_tags = word_div.find_all('letter')
+    letters = ''.join(letter_tag.string for letter_tag in letter_tags)
+    return letters
+
+
+def get_wordlist_from_file(filename):
+    with open('test.txt', 'r') as f:
+        html_doc = ''.join(f.readlines())
+
+    # Find words class in HTML
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    word_divs_wrapper = soup.find('div', {'class': 'words'})
+    all_word_divs = word_divs_wrapper.find_all('div', {'class': 'word'})
+
+    # Add all words to wordlist
+    wordlist = []
+    for word_div in all_word_divs:
+        wordlist.append(Word(
+            letters=get_word_from_div(word_div),
+            wpm=word_div.get('burst', None),
+            is_error=('error' in word_div.get('class', None))
+        ))
+    
+    return wordlist
+
+
+    
